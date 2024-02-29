d2e461d902b792b39ad304e7b0243050d1184474
Albert Joel Bonilla Guerrero <albertjoelbg@gmail.com>
2024-02-29 08:41:14 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "../app.pcss";
+    import {Navbar, NavBrand, NavLi, NavUl, NavHamburger} from 'flowbite-svelte';
+</script>
+<Navbar>
+    <NavBrand href="/">
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger/>
+    <NavUl>
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/nosotros">Nosotros</NavLi>
+        <NavLi href="/docs/components/navbar">Navbar</NavLi>
+        <NavLi href="/pricing">Pricing</NavLi>
+        <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+</Navbar>
+<slot></slot>
---
+++ src/routes/nosotros/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-2xl font-semibold text-center">Nosotros</h1>
