4be1aa74388168c1181944391c13bea1807d4c6f
Albert Joel Bonilla Guerrero <albertjoelbg@gmail.com>
2024-11-03 12:51:05 -0500

    index finished

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,35 +1,12 @@
-// An array of links for navigation bar
 const navBarLinks = [
   { name: "Inicio", url: "/" },
-  { name: "Productos", url: "/productos" },
   { name: "Servicios", url: "/servicios" },
   { name: "Blog", url: "/blog" },
   { name: "Contacto", url: "/contacto" },
 ];
-// An array of links for footer
-const footerLinks = [
-  {
-    section: "Ecosystem",
-    links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/productos" },
-      { name: "Construction Services", url: "/servicios" },
-    ],
-  },
-  {
-    section: "Company",
-    links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "/blog" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
-    ],
-  },
-];
-// An object of links for social icons
+
 const socialLinks = {
-  facebook: "https://www.facebook.com/",
-  x: "https://twitter.com/",
+  facebook: "https://www.facebook.com/sociedad.motupe",
   github: "https://github.com/mearashadowfax/ScrewFast",
   google: "https://www.google.com/",
   slack: "https://slack.com/",
@@ -37,6 +14,5 @@ const socialLinks = {
 
 export default {
   navBarLinks,
-  footerLinks,
   socialLinks,
 };
